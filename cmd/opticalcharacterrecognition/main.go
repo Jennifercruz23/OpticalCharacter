6d9f5319b77abb49322222ac682368c3e18c882c
Jennifer <142938327+Jennifercruz23@users.noreply.github.com>
2025-08-24 11:49:28 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/opticalcharacterrecognition/main.go
@@ -0,0 +1,23 @@
+// cmd/opticalcharacterrecognition/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "opticalcharacterrecognition/internal/opticalcharacterrecognition"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := opticalcharacterrecognition.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
